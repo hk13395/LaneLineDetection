ae87f5b9543f573dc63e4b32b53f0c987cd36969
Harshal Kolhe <harshalkolhe90@gmail.com>
2021-07-29 22:43:18 -0400

    final outputs and script

--- LaneDetecttion.py
+++ LaneDetecttion.py
@@ -4,7 +4,10 @@ import numpy as np
 import cv2
 import os
 from helper_function import*
+from IPython.display import HTML
+from moviepy.editor import VideoFileClip
 
+# implementing on test images before videos
 test_path = os.listdir("test images/")
 
 test_output = "test_output_images"
@@ -15,5 +18,25 @@ for test_path in test_path:
     test_img = mpimg.imread(os.path.join('test images', test_path))
     get_momentum()
     final_output = process_image(test_img)
-    plt.imshow(final_output)
-    plt.show()
+    # plt.imshow(final_output)
+    # plt.show()
+
+
+# implementing on video file
+white_output = 'test_video_output/solidWhiteRight.mp4'
+clip1 = VideoFileClip("test videos/solidWhiteRight.mp4")
+white_clip = clip1.fl_image(process_image)
+white_clip.write_videofile(white_output, audio=False)
+
+HTML("""<video width="960" height="540" control>
+<source src="{0}">
+</video>""".format(white_output))
+
+yellow_output = 'test_video_output/solidYellowLeft.mp4'
+clip2 = VideoFileClip("test videos/solidYellowLeft.mp4")
+yellow_clip = clip2.fl_image(process_image)
+yellow_clip.write_videofile(yellow_output, audio=False)
+
+HTML("""<video width="960" height="540" control>
+<source src="{0}">
+</video>""".format(yellow_output))
